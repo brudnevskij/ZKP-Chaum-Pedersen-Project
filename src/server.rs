9e500971c7ad3cdd0cd9880cc54961d859b2fb63
Segfaulter <brudnevskij@users.noreply.github.com>
2025-01-12 01:35:09 +0200

    Implemented create auth handler
    Implemented create auth challenge handler, added auth session to user
    storage

--- src/server.rs
+++ src/server.rs
@@ -6,17 +6,19 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::sync::Mutex;
 use num_bigint::{BigInt, BigUint};
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::{transport::Server, Code, Request, Response, Status};
 use tonic::codegen::ok;
 use zkp_auth::{
     auth_server::{Auth, AuthServer},
     AuthenticationAnswerRequest, AuthenticationAnswerResponse, AuthenticationChallengeRequest,
     AuthenticationChallengeResponse, RegisterRequest, RegisterResponse,
 };
+use zkp_auth_project::ZKP;
 
 #[derive(Debug, Default)]
 pub struct AuthImpl {
-    pub user_info: Mutex<HashMap<String,UserInfo>>
+    pub user_info: Mutex<HashMap<String,UserInfo>>,
+    pub auth_id_to_user: Mutex<HashMap<String,String>>
 }
 
 #[derive(Debug, Default)]
@@ -62,7 +64,26 @@ impl Auth for AuthImpl {
         &self,
         request: Request<AuthenticationChallengeRequest>,
     ) -> Result<Response<AuthenticationChallengeResponse>, Status> {
-        todo!()
+        let request = request.into_inner();
+        print!("Request: {:?}", request);
+        let user_name = request.user;
+        let mut user_info_storage = &mut self.user_info.lock().unwrap();
+
+        if let Some(user_info) = user_info_storage.get_mut(&user_name) {
+            user_info.r1 = BigUint::from_bytes_be(&request.r1);
+            user_info.r2 = BigUint::from_bytes_be(&request.r2);
+
+            let (_,_,_,q) = ZKP::get_constants();
+            let c  = ZKP::generate_random_below(&q);
+            let auth_id = "skdjfsk".to_string();
+
+            let auth_id_storage = &mut self.auth_id_to_user.lock().unwrap();
+            auth_id_storage.insert(auth_id.clone(), user_info.user_name.clone());
+
+            Ok(Response::new(AuthenticationChallengeResponse{auth_id, c: c.to_bytes_be()}))
+        } else {
+            Err(Status::new(Code::NotFound, "User not found".to_string()))
+        }
     }
     async fn verify_authentication(
         &self,
